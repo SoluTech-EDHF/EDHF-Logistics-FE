4ff766fb6bce6d49d170fd1e3db67ffef32f8bc5
ALADETAN-IFE <famousfotune123@gmail.com>
2025-12-27 10:45:34 +0100

    fix(interceptors): simplify token retrieval in registerInterceptors

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,7 @@ import App from "./App.tsx";
 import { useAppStore } from "@/store";
 import { registerInterceptors } from "@/services";
 
-registerInterceptors(
-  () => useAppStore.getState().token ?? useAppStore.getState()?.token,
-);
+registerInterceptors(() => useAppStore.getState().token);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
